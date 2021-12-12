e8650bdc72ba2feceb04b7786c389acc67abe6b3
Pierre Adel <pierreadelkamel@gmail.com>
2021-12-12 16:53:38 +0200

    Completed LoginStudent page, changed font and commented most global CSS

--- src/Pages/LoginStudent/LoginStudent.js
+++ src/Pages/LoginStudent/LoginStudent.js
@@ -1,65 +1,73 @@
-import React from 'react';
-import {ReactComponent as Logo} from '../../assets/instagram.svg'
-import './LoginStudent.css';
-import { useNavigate } from 'react-router-dom';
-import Home from '../Home/Home';
-import axios from 'axios';
+import React from "react";
+import "./LoginStudent.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-function LoginStudent (){
-    const navigate=  useNavigate();
-    // const state={
-    //     email:'',
-    //     pwd:''
-    // }
-    
-    // function handleChange(e){
-        
-    //     const {name,value} = e.target
-    //     // this.setState({[name]:value})
-    // }
+function LoginStudent() {
+  const navigate = useNavigate();
+  // const state={
+  //     email:'',
+  //     pwd:''
+  // }
 
+  // function handleChange(e){
 
-   function handleS(e){
-        e.preventDefault();
-          
-        // this.props.isLogin(true)
-        axios.post(
-            `http://127.0.0.1:3008/api/student/login`,{
-                username:e.target.username.value,
-                password:e.target.password.value
-            }
-        ).then((response)=>{
-            if(response.status==400){
-                alert(response.data)
-            }else{
-                navigate(`/quizzes/${response.data.id}`);
-            }
-        })
+  //     const {name,value} = e.target
+  //     // this.setState({[name]:value})
+  // }
 
-    }
-        return(
-            <div className='div-login'>
-                 <div class="card">
-    <div class="card-block">
-            <h2 class="card-header text-center">Ain Shams University - Faculty of Engineering</h2></div></div>
-         
-                <div className='div-login-logo'>
-                 <head>     <base href="/"/> </head>
-        <img id="pic" src="./images/lms.png"/>
-                </div>
-                <div>
-                    <form id="form" onSubmit = {handleS  }  >
-                        <input name='username' placeholder='username...' required />
-                        <input type='password' name='password' placeholder='password...' /><br></br>
-                        <button>Log In</button>
-                    </form>
-                    <footer>
-        <p>Online Quiz System, Copyright &copy; 2021</p>
-      </footer>
-                </div>
-                  </div>
-        )
-    }
+  function handleS(e) {
+    e.preventDefault();
 
+    // this.props.isLogin(true)
+    axios
+      .post(`http://127.0.0.1:3008/api/student/login`, {
+        username: e.target.username.value,
+        password: e.target.password.value,
+      })
+      .then((response) => {
+        if (response.status === 400) {
+          alert(response.data);
+        } else {
+          navigate(`/quizzes/${response.data.id}`);
+        }
+      });
+  }
+  return (
+    <div className="div-background">
+      <div className="div-login">
+        <p className="title">Welcome Back</p>
+        {/* to maintain constant width of elements */}
+        <div className="middle-container">
+          <img
+            className="slogin-pic"
+            id="pic"
+            src="./images/lms.png"
+            alt="lms"
+          />
+          <form className="slogin-form" id="form" onSubmit={handleS}>
+            <input
+              className="slogin-input"
+              name="username"
+              placeholder="Username"
+              required
+            />
+            <input
+              className="slogin-input"
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <button className="button">Login</button>
+          </form>
+          <p className="last-text">
+            Online Quiz System, Copyright &#x24B8; {new Date().getFullYear()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default LoginStudent;
